6cc0da3220a964ebe3b19dd86f8546d6eba09681
devanandersen <devanandersenbusiness@gmail.com>
2019-03-20 14:41:22 -0400

    Updates

---
+++ precache-manifest.ca1337e0028a2da8eaf337b843ed12ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a636f9f010ba12dab7c",
+    "url": "/reactquotes/static/js/runtime~main.e25eb27f.js"
+  },
+  {
+    "revision": "8787d5595a421c2fae1f",
+    "url": "/reactquotes/static/js/main.45a43a1e.chunk.js"
+  },
+  {
+    "revision": "0dfc3aa9cbb30f28b5e6",
+    "url": "/reactquotes/static/js/2.f20903f0.chunk.js"
+  },
+  {
+    "revision": "8787d5595a421c2fae1f",
+    "url": "/reactquotes/static/css/main.b8de6c5e.chunk.css"
+  },
+  {
+    "revision": "0659f414e05b84a6003b482145086feb",
+    "url": "/reactquotes/index.html"
+  }
+];
